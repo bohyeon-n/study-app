9c8a8850c5a4bd5c1606622ae09f9fb3ef103a94
koo bohyeon <goosee115@gmail.com>
2020-08-18 16:57:23 +0900

    fix: 스터디 카테고리 분류 수정함

--- study-app/src/pages/PostUpdate.tsx
+++ study-app/src/pages/PostUpdate.tsx
@@ -46,19 +46,24 @@ const Buttons = styled.div`
 
 export const PostUpdate = () => {
   const options: OptionType<string>[] = [
-    { label: 'java', value: 'java' },
-    { label: 'javascript', value: 'javascript' },
-    { label: 'haha', value: 'haha' },
-    { label: 'hoho', value: 'hoho' }
+    { label: '언어', value: '언어' },
+    { label: 'CS', value: 'CS' },
+    { label: '취업', value: '취업' },
+    { label: '기타', value: '기타' }
   ]
 
   const locationOptions: OptionType<string>[] = [
-    { label: '충남', value: '충남' },
-    { label: '경기', value: '경기' },
     { label: '서울', value: '서울' },
-    { label: '부산', value: '부산' },
-    { label: '광주', value: '광주' },
-    { label: '강원도', value: '강원도' }
+    { label: '경기', value: '경기' },
+    { label: '인천', value: '인천' },
+    { label: '대전/충청/세종', value: '대전/충청/종종' },
+    { label: '부산/울산/경남', value: '부산/울산/경남' },
+    { label: '광주/전라', value: '광주/전라' },
+    { label: '대구/경북', value: '대구/경북' },
+    { label: '강원', value: '강원' },
+    { label: '제주', value: '제주' },
+    { label: '기타', value: '기타' },
+    { label: '온라인', value: '온라인' }
   ]
 
   const { postId } = useParams()
