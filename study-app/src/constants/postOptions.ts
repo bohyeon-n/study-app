2568da3c480fa22e9da8c25fbf20c5b7379caf44
koo bohyeon <goosee115@gmail.com>
2020-09-08 14:38:42 +0900

    feature: 카테고리 수정
    스터디 카테고리 옵션 수정함

--- study-app/src/constants/postOptions.ts
+++ study-app/src/constants/postOptions.ts
@@ -1,17 +1,23 @@
 import { OptionType } from '../style-components/selectBox/OptionType'
 
 export const categoryOptions: OptionType<string>[] = [
-  { label: '언어', value: '언어' },
-  { label: 'CS', value: 'CS' },
+  { label: '프로그래밍', value: '프로그래밍' },
+  { label: '디자인', value: '디자인' },
+  { label: '외국어', value: '외국어' },
+  { label: '취미/생활', value: '취미/생활' },
+  { label: '스포츠/건강', value: '스포츠/건강' },
   { label: '취업', value: '취업' },
-  { label: '기타', value: '기타' }
+  { label: '시험/자격증', value: '시험/자격증' },
+  { label: '투자/부업/N잡', value: '투자/부업/N잡' },
+  { label: '미술', value: '미술' },
+  { label: '악기', value: '악기' }
 ]
 
 export const locationOptions: OptionType<string>[] = [
   { label: '서울', value: '서울' },
   { label: '경기', value: '경기' },
   { label: '인천', value: '인천' },
-  { label: '대전/충청/세종', value: '대전/충청/종종' },
+  { label: '대전/충청/세종', value: '대전/충청/세종' },
   { label: '부산/울산/경남', value: '부산/울산/경남' },
   { label: '광주/전라', value: '광주/전라' },
   { label: '대구/경북', value: '대구/경북' },
