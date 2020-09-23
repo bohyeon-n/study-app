50bbad27706311e953f768c51732c0374d833d2b
koo bohyeon <goosee115@gmail.com>
2020-09-24 01:02:15 +0900

    feature: database Image 테이블 추가함

--- study-app-server/src/main/resources/schema.sql
+++ study-app-server/src/main/resources/schema.sql
@@ -5,29 +5,37 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 (
-    id       bigint primary key auto_increment,
-    email    varchar(512),
-    username varchar(512),
+    id          bigint primary key auto_increment,
+    email       varchar(512),
+    username    varchar(512),
     profile_url varchar(512)
 );
 
 CREATE TABLE post
 (
-    id         bigint primary key auto_increment,
-    title      varchar(512),
-    location      varchar(128),
-    category   varchar(128),
+    id           bigint primary key auto_increment,
+    title        varchar(512),
+    location     varchar(128),
+    category     varchar(128),
     created_time datetime,
-    content    text,
-    view_count bigint,
+    content      text,
+    view_count   bigint,
     author_id    bigint references user (id)
 );
 
 CREATE TABLE comment
 (
-    id bigint primary key auto_increment,
-    content text,
-    post_id bigint references post(id),
-    author_id bigint references user(id),
+    id           bigint primary key auto_increment,
+    content      text,
+    post_id      bigint references post (id),
+    author_id    bigint references user (id),
     created_time datetime
-)
+);
+
+CREATE TABLE IMAGE
+(
+    id      bigint primary key auto_increment,
+    name    varchar(512),
+    type    varchar(128),
+    pic_byte MEDIUMBLOB
+);
