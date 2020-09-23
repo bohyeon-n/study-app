8e807834684835d2057c9bf9ed9315bcd1cbad80
koo bohyeon <goosee115@gmail.com>
2020-09-24 01:03:28 +0900

    feature: 이미지 추가, 가져오기 API 추가함
    글쓰기 기능에서 이미지도 추가하기 위해서
    사용자가 업로드한 이미지를 저장하고, 이미지 id로 이미지를 가져오는 API 추가함

---
+++ study-app-server/src/main/java/com/bohyeon/studyapp/file/FileUploadController.java
@@ -0,0 +1,74 @@
+package com.bohyeon.studyapp.file;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+@RestController
+public class FileUploadController {
+
+    FileService fileService;
+
+    public FileUploadController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/images")
+    public Long uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        Image image = new Image(file.getName(), file.getContentType(), compressBytes(file.getBytes()));
+        System.out.println(image);
+        return fileService.addImage(image);
+    }
+
+    @GetMapping("/images/{id}")
+    public byte[] getImage(@PathVariable Long id, HttpServletResponse response) {
+        Image image = fileService.getImageById(id);
+        image.setPicByte(decompressBytes(image.getPicByte()));
+        response.setContentType(image.getType());
+
+        return image.getPicByte();
+    }
+
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                System.out.println("compressed Image Byte Size -" + outputStream.toByteArray().length);
+            }
+        }
+        return outputStream.toByteArray();
+    }
+
+    public static byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException ignored) {
+        }
+
+        return outputStream.toByteArray();
+    }
+}
