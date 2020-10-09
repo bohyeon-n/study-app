1d5f991d2945baabfa2ad36628fb1c70fd218a11
koo bohyeon <goosee115@gmail.com>
2020-10-09 15:47:11 +0900

    test: build test

--- study-app-server/deploy.sh
+++ study-app-server/deploy.sh
@@ -14,12 +14,12 @@ then
   echo "> 종료할 것 없음." >> /home/ubuntu/deploy.log
 else
   echo "> sudo kill -15 $CURRENT_PID" >> /home/ubuntu/deploy.log
-  kill -15 "$CURRENT_PID"
+  kill -15 "$CURRENT_PID" >> /home/ubuntu/deploy.log 2>&1
   sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.config.location=classpath:/,file:///home/ubuntu/study-app-env/application-prod.properties "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.config.location=classpath:/,file:///home/ubuntu/study-app-env/application-prod.properties "$JAR_PATH" >> /home/ubuntu/deploy.log 2>&1 &
 
 echo "[$(date)] server deploy" >> /home/ubuntu/deploy.log
 
